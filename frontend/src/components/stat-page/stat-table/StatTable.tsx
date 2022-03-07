2c990653c2fc4a911ab1dfe9bd7f7e13f5a5e898
Patrick Ubelhor <pubelhor@gmail.com>
2022-03-07 15:47:05 -0600

    Added sorting to table

--- frontend/src/components/stat-page/stat-table/StatTable.tsx
+++ frontend/src/components/stat-page/stat-table/StatTable.tsx
@@ -1,17 +1,42 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel } from '@material-ui/core';
 import React from 'react';
 import { TeamObjectiveStats } from '../../../models/response.model';
 import { useTranslator } from '../../../service/TranslateService';
 
+enum Statistic {
+	TEAM_NUMBER = 'teamNumber',
+	MEAN = 'mean',
+	MEDIAN = 'median',
+	MODE = 'mode'
+}
+
+enum Order {
+	ASCENDING = 'asc',
+	DESCENDING = 'desc'
+}
+
 interface IProps {
-	data: TeamObjectiveStats[]
+	data: TeamObjectiveStats[];
 }
 
 export default function StatTable({ data }: IProps) {
 
 	const translate = useTranslator();
+	const [orderDirection, setOrderDirection] = React.useState(Order.DESCENDING);
+	const [orderProperty, setOrderProperty] = React.useState(Statistic.MEAN);
 
-	const sortedData = data.slice().sort((a: TeamObjectiveStats, b: TeamObjectiveStats) => b.mean - a.mean);
+	const sortedData = data.slice().sort((a: TeamObjectiveStats, b: TeamObjectiveStats) => {
+		const comparison = a[orderProperty] - (b[orderProperty]);
+		return (orderDirection === Order.ASCENDING) ? comparison : -comparison;
+	});
+
+	const handleSort = (property: Statistic) => {
+		// Should sort 'descending' if we change properties or were previously 'ascending'
+		const nextOrderIsDesc = (orderProperty !== property) || (orderDirection === Order.ASCENDING);
+		setOrderDirection(nextOrderIsDesc ? Order.DESCENDING : Order.ASCENDING);
+		setOrderProperty(property);
+	}
+
 	const rows = sortedData.map((teamData: TeamObjectiveStats) => (
 		<TableRow key={teamData.teamNumber}>
 			<TableCell align="left">{ teamData.teamNumber }</TableCell>
@@ -24,38 +49,80 @@ export default function StatTable({ data }: IProps) {
 	return (
 		<TableContainer>
 			<Table aria-label={ translate('STATS_TABLE') }>
-				<TableHead>
-					<TableRow>
-						<TableCell
-							className="stat-table-cell"
-							align="left"
-						>
-							{ translate('TEAM_NUMBER') }
-						</TableCell>
-						<TableCell
-							className="stat-table-cell"
-							align="right"
-						>
-							{ translate('MEAN') }
-						</TableCell>
-						<TableCell
-							className="stat-table-cell"
-							align="right"
-						>
-							{ translate('MEDIAN') }
-						</TableCell>
-						<TableCell
-							className="stat-table-cell"
-							align="right"
-						>
-							{ translate('MODE') }
-						</TableCell>
-					</TableRow>
-				</TableHead>
+				<EnhancedTableHead
+					sortDirection={orderDirection}
+					sortProperty={orderProperty}
+					onSort={handleSort}
+				/>
 				<TableBody>
 					{ rows }
 				</TableBody>
 			</Table>
 		</TableContainer>
+	);
+}
+
+interface ITableHeadProps {
+	sortDirection: Order;
+	sortProperty: Statistic;
+	onSort: (property: Statistic) => void;
+}
+
+function EnhancedTableHead(props: ITableHeadProps) {
+	const translate = useTranslator();
+
+	return (
+		<TableHead>
+			<TableRow>
+				<TableCell
+					className="stat-table-cell"
+					align="left"
+				>
+					{ translate('TEAM_NUMBER') }
+				</TableCell>
+				<TableCell
+					className="stat-table-cell"
+					align="right"
+					sortDirection={props.sortProperty === Statistic.MEAN ? props.sortDirection : false}
+				>
+					<TableSortLabel
+						active={props.sortProperty === Statistic.MEAN}
+						direction={props.sortProperty === Statistic.MEAN ? props.sortDirection : Order.ASCENDING}
+						onClick={(event) => {
+							console.log(event);
+							props.onSort(Statistic.MEAN);
+						}}
+					>
+						{ translate('MEAN') }
+					</TableSortLabel>
+				</TableCell>
+				<TableCell
+					className="stat-table-cell"
+					align="right"
+					sortDirection={props.sortProperty === Statistic.MEDIAN ? props.sortDirection : false}
+				>
+					<TableSortLabel
+						active={props.sortProperty === Statistic.MEDIAN}
+						direction={props.sortProperty === Statistic.MEDIAN ? props.sortDirection : Order.ASCENDING}
+						onClick={() => props.onSort(Statistic.MEDIAN)}
+					>
+						{ translate('MEDIAN') }
+					</TableSortLabel>
+				</TableCell>
+				<TableCell
+					className="stat-table-cell"
+					align="right"
+					sortDirection={props.sortProperty === Statistic.MODE ? props.sortDirection : false}
+				>
+					<TableSortLabel
+						active={props.sortProperty === Statistic.MODE}
+						direction={props.sortProperty === Statistic.MODE ? props.sortDirection : Order.ASCENDING}
+						onClick={() => props.onSort(Statistic.MODE)}
+					>
+						{ translate('MODE') }
+					</TableSortLabel>
+				</TableCell>
+			</TableRow>
+		</TableHead>
 	);
 }

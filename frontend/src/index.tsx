ff1999dfaae517827d5c3fcd1544f20cb42df129
Patrick Ubelhor <pubelhor@gmail.com>
2023-02-17 01:20:58 -0600

    Upgraded react

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import { store } from './state/Store';
 
 // Inject the App component into the index.html
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App/>
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+  </Provider>
+)

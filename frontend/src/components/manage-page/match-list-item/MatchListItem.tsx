3663d6a38f145b2630c8eea326b8eb52d909d572
PatrickUbelhor <pubelhor@gmail.com>
2023-04-05 15:46:15 -0500

    Added chevron to mobile match list

--- frontend/src/components/manage-page/match-list-item/MatchListItem.tsx
+++ frontend/src/components/manage-page/match-list-item/MatchListItem.tsx
@@ -1,4 +1,5 @@
 import './MatchListItem.scss';
+import { Icon } from '@mui/material';
 import React from 'react';
 import { Match } from '../../../models';
 import { useTranslator } from '../../../service/TranslateService';
@@ -11,17 +12,26 @@ interface IProps {
 export default function MatchListItem(props: IProps) {
 	const translate = useTranslator();
 
+	const chevron = (props.isMobile)
+		? (
+			<div className="chevron">
+				<Icon color="primary">chevron_right</Icon>
+			</div>
+		)
+		: null;
+
 	return (
 		<div className={
 			'match-list-item'
 			+ (props.isMobile ? ' match-list-item-mobile' : '')
 			+ (props.match.isHidden ? ' hidden' : '')
 		}>
-			<div className="match-number">{ translate('MATCH') } { props.match.matchNumber }</div>
-			<div className="bottom-row">
+			<div className="left-info">
+				<div className="match-number">{ translate('MATCH') } { props.match.matchNumber }</div>
 				<div className="robot-number">{ translate('TEAM') } { props.match.robotNumber }</div>
-				<div className="creator">{ props.match.creator }</div>
 			</div>
+			<div className="creator">{ props.match.creator }</div>
+			{ chevron }
 		</div>
 	);
 }

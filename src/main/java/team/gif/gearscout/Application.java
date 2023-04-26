fa36efc7747f407982593d514f20bfb5c861e5a2
PatrickUbelhor <pubelhor@gmail.com>
2023-04-25 19:01:10 -0500

    Fixed some CORS stuff

--- src/main/java/team/gif/gearscout/Application.java
+++ src/main/java/team/gif/gearscout/Application.java
@@ -19,7 +19,7 @@ public class Application {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-					.allowedOriginPatterns("http://localhost:*")
+					.allowedOriginPatterns("http://localhost:[*]", "https://*.gearitforward.com")
 					.allowedHeaders("*")
 					.allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE")
 					.exposedHeaders("*")

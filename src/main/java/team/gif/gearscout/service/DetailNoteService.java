31936d2651ea432725187510807842e1c7ae3465
PatrickUbelhor <pubelhor@gmail.com>
2023-07-06 14:53:17 -0500

    Bumped version number

--- src/main/java/team/gif/gearscout/service/DetailNoteService.java
+++ src/main/java/team/gif/gearscout/service/DetailNoteService.java
@@ -11,8 +11,6 @@ import javax.transaction.Transactional;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional

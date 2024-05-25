c6657c9cca0273c15bcb6b78aa53e2cbd32802a2
Patrick Ubelhor <pubelhor@gmail.com>
2024-05-25 17:24:08 -0500

    Fixed CORS settings

--- src/main/java/team/gif/gearscout/Application.java
+++ src/main/java/team/gif/gearscout/Application.java
@@ -3,13 +3,20 @@ package team.gif.gearscout;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -33,26 +40,11 @@ public class Application {
 		};
 	}
 
-//	@Bean
-//	public WebSecurityConfigurer configureWebSecurity() {
-//		return new WebSecurityConfigurer() {
-//			@Override
-//			protected void configure(HttpSecurity http) throws Exception {
-//				http.authorizeHttpRequests()
-//					.anyRequest()
-//					.permitAll()
-//					.and()
-//					.cors()
-//					.and()
-//					.csrf().disable();
-//			}
-//		}
-//	}
-
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll())
-			.csrf(AbstractHttpConfigurer::disable);
+			.csrf(AbstractHttpConfigurer::disable)
+			.cors(Customizer.withDefaults());
 		return http.build();
 	}
 

c1f34867108c991eb942485fa8ed3372757d3aa0
Patrick Ubelhor <pubelhor@gmail.com>
2024-12-18 20:23:49 -0600

    Added account button

--- pitscout/src/components/header/Header.tsx
+++ pitscout/src/components/header/Header.tsx
@@ -1,28 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.scss';
-import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import {
+	AppBar,
+	Icon,
+	IconButton,
+	ListItemIcon, ListItemText,
+	Menu,
+	MenuItem,
+	Toolbar,
+	Tooltip,
+	Typography
+} from '@mui/material';
 import { logout, selectIsLoggedIn, useAppDispatch, useAppSelector } from '../../state';
+import { ExitToApp } from '@mui/icons-material';
+import ProfileCard from './profile-card/ProfileCard';
 
 export default function Header() {
 	const isLoggedIn: boolean = useAppSelector(selectIsLoggedIn);
 
 	const dispatch = useAppDispatch();
-	const _logout = () => dispatch(logout());
 
-	const logoutButton = (
-		<Button
-			className="logout-button"
-			color="primary"
-			disableElevation={true}
-			variant="contained"
-			aria-label="Logout"
-			onClick={_logout}
-			sx={{
-				marginLeft: 'auto'
-			}}
+	const [accountAnchor, setAccountAnchor] = useState(null);
+	const handleAccountMenuClick = (event) => setAccountAnchor(event.currentTarget);
+	const handleAccountMenuClose = () => setAccountAnchor(null);
+
+	const _logout = () => {
+		handleAccountMenuClose();
+		dispatch(logout());
+	};
+
+	const accountButton = (
+		<Tooltip title="Account">
+			<IconButton
+				id="account-button"
+				edge="end"
+				color="inherit"
+				aria-label="Account"
+				aria-controls="account-menu"
+				aria-haspopup="true"
+				onClick={ handleAccountMenuClick }
+			>
+				<Icon>account_circle</Icon>
+			</IconButton>
+		</Tooltip>
+	);
+
+	const accountMenu = (
+		<Menu
+			id="account-menu"
+			anchorEl={ accountAnchor }
+			open={ Boolean(accountAnchor) }
+			onClose={ handleAccountMenuClose }
+			keepMounted={ true }
 		>
-			Logout
-		</Button>
+			<ProfileCard sx={{ margin: '8px 12px' }}/>
+			{ /* TODO: Change event button */ }
+			<MenuItem onClick={ _logout }>
+				<ListItemIcon>
+					<ExitToApp />
+				</ListItemIcon>
+				<ListItemText>
+					Logout
+				</ListItemText>
+			</MenuItem>
+		</Menu>
 	);
 
 	return (
@@ -40,7 +81,8 @@ export default function Header() {
 					</Typography>
 					<span>{ process.env.REACT_APP_VERSION }</span>
 				</div>
-				{ isLoggedIn ? logoutButton : null }
+				{ isLoggedIn && accountButton }
+				{ isLoggedIn && accountMenu }
 			</Toolbar>
 		</AppBar>
 	);

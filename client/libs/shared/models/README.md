445591a437c9d19ecc92ffce4a7bac5d8c901d06
Patrick Ubelhor <pubelhor@gmail.com>
2024-12-26 23:31:50 -0600

    Converted libs to vitest

--- client/libs/shared/models/README.md
+++ client/libs/shared/models/README.md
@@ -2,10 +2,6 @@
 
 This library was generated with [Nx](https://nx.dev).
 
-## Building
-
-Run `nx build gearscout-shared-models` to build the library.
-
 ## Running unit tests
 
-Run `nx test gearscout-shared-models` to execute the unit tests via [Jest](https://jestjs.io).
+Run `nx test gearscout-shared-models` to execute the unit tests via [Vitest](https://vitest.dev/).
--- client/libs/shared/models/eslint.config.cjs
+++ client/libs/shared/models/eslint.config.cjs
@@ -1,19 +1,12 @@
+const nx = require('@nx/eslint-plugin');
 const baseConfig = require('../../../eslint.config.cjs');
 
 module.exports = [
 	...baseConfig,
+	...nx.configs['flat/react'],
 	{
-		files: ['**/*.json'],
-		rules: {
-			'@nx/dependency-checks': [
-				'error',
-				{
-					ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}'],
-				},
-			],
-		},
-		languageOptions: {
-			parser: require('jsonc-eslint-parser'),
-		},
+		files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+		// Override or add rules here
+		rules: {},
 	},
 ];

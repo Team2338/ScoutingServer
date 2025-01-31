e39250ec344dff5a8a7bf7d058d1aad8c4ab35fc
Patrick Ubelhor <pubelhor@gmail.com>
2025-01-31 13:43:43 -0500

    Added confirmation dialog

--- client/apps/pitscout/src/components/main-page/robot-list/RobotList.tsx
+++ client/apps/pitscout/src/components/main-page/robot-list/RobotList.tsx
@@ -20,25 +20,23 @@ export default function RobotList() {
 	const isMobile: boolean = useMediaQuery('(max-width: 700px)');
 
 	const listOptions = robotNumbers.map((robot: number) => (
-		<Fragment>
-			<ListItemButton
-				key={ robot }
-				id={ 'robot-list-item-' + robot }
-				selected={ robot === selectedRobot }
-				onClick={ () => dispatch(selectForm(robot)) }
-				sx={{
-					paddingTop: '12px',
-					paddingBottom: '12px',
-					fontWeight: robot === selectedRobot ? 600 : 400,
-					borderRadius: '12px',
-					width: '100%',
-					justifyContent: 'center',
-					border: isMobile ? '2px solid #1976d220' : 'none'
-				}}
-			>
-				<span className="robot-list-item__number">{ robot }</span>
-			</ListItemButton>
-		</Fragment>
+		<ListItemButton
+			key={ robot }
+			id={ 'robot-list-item-' + robot }
+			selected={ robot === selectedRobot }
+			onClick={ () => dispatch(selectForm(robot)) }
+			sx={{
+				paddingTop: '12px',
+				paddingBottom: '12px',
+				fontWeight: robot === selectedRobot ? 600 : 400,
+				borderRadius: '12px',
+				width: '100%',
+				justifyContent: 'center',
+				border: isMobile ? '2px solid #1976d220' : 'none'
+			}}
+		>
+			<span className="robot-list-item__number">{ robot }</span>
+		</ListItemButton>
 	));
 
 	return (
--- client/libs/shared/components/src/index.ts
+++ client/libs/shared/components/src/index.ts
@@ -1,2 +1,3 @@
+export * from './confirmation-modal/ConfirmationModal';
 export * from './event-selector-list/EventSelectorList';
 export * from './profile-card/ProfileCard';

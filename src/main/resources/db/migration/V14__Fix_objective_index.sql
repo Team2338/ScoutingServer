42d79db8407cfc21172982e9ebcf06588a196d4e
Patrick Ubelhor <pubelhor@gmail.com>
2025-03-15 12:47:12 -0400

    Add 'if exists' condition

--- src/main/resources/db/migration/V14__Fix_objective_index.sql
+++ src/main/resources/db/migration/V14__Fix_objective_index.sql
@@ -6,5 +6,5 @@ CREATE INDEX IF NOT EXISTS idx__match_obj__match_id
 -- We never look up a match ID from an obj ID
 -- This will improve write performance
 ALTER TABLE matches_objectives
-	DROP CONSTRAINT uk_ddtkqjem3iy6vku1meenj2tsp
+	DROP CONSTRAINT IF EXISTS uk_ddtkqjem3iy6vku1meenj2tsp
 ;

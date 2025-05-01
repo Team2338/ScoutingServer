3176a6ebc2368a49041b1cef2334941c54c1c0d6
Evan Perry Grove <evan@4grove.com>
2025-05-01 07:50:16 -0500

    index: Implement dark theme palette

--- client/apps/analytics/src/index.tsx
+++ client/apps/analytics/src/index.tsx
@@ -5,13 +5,38 @@ import { BrowserRouter } from 'react-router';
 import App from './components/App';
 import { store } from './state';
 import { myRegister, unregister } from './serviceWorkerRegistration';
-import { createTheme, ThemeProvider } from '@mui/material';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { orange } from '@mui/material/colors';
 
+const myBlue = {
+	900: '#254999',
+	800: '#2f67b9',
+	700: '#3478cb',
+	600: '#3b8ade',
+	500: '#4198eb',
+	400: '#55a7ef',
+	300: '#70b6f1',
+	200: '#96cbf6',
+	100: '#bedff9',
+	50: '#e4f2fc'
+}
+
 const theme = createTheme({
+	colorSchemes: {
+		dark: {
+			palette: {
+				primary: myBlue,
+				secondary: orange,
+				background: {
+					default: '#18191b'
+				}
+			},
+		}
+	},
 	palette: {
 		primary: {
-			main: '#254999'
+			main: myBlue[900],
+			light: myBlue[500]
 		},
 		secondary: orange
 	}
@@ -23,6 +48,7 @@ root.render(
 	<Provider store={ store }>
 		<BrowserRouter>
 			<ThemeProvider theme={theme}>
+				<CssBaseline/>
 				<App/>
 			</ThemeProvider>
 		</BrowserRouter>

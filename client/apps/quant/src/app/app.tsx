72345dc38533ddc6805b8c0dff33350cbc53c3ea
Patrick Ubelhor <pubelhor@gmail.com>
2026-02-20 18:45:00 -0500

    Get schedule on login

--- client/apps/quant/src/app/app.tsx
+++ client/apps/quant/src/app/app.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import LoginPage from '../components/login-page/LoginPage';
-import { ICredentials, IMatch } from '../models/models';
+import { ICredentials, IMatch, IMatchLineup } from '../models/models';
 import DataCollectionPage from '../components/data-collection-page/DataCollectionPage';
 import { gearscoutService } from '../services/GearscoutService';
 
 export function App() {
 	const [credentials, setCredentials] = useState<ICredentials | null>(null);
+	const [scheduleIsLoading, setScheduleIsLoading] = useState<boolean>(false);
+	const [schedule, setSchedule] = useState<IMatchLineup[] | null>(null);
 
 	const submitMatchData = (match: IMatch): void => {
 		console.log('Submitting match data', match);
@@ -18,17 +20,41 @@ export function App() {
 			});
 	};
 
+	const handleLogin = (credentials: ICredentials): void => {
+		setCredentials(credentials);
+
+		if (credentials.tbaCode.trim().length === 0) {
+			return;
+		}
+
+		setScheduleIsLoading(true);
+		gearscoutService.getEventSchedule(2026, credentials.tbaCode)
+			.then((response) => {
+				const res: IMatchLineup[] = response.data;
+				console.log('Got schedule', res);
+				setSchedule(res);
+				setScheduleIsLoading(false);
+			})
+			.catch((error) => {
+				setScheduleIsLoading(false);
+				console.error('Failed to get schedule', error);
+			});
+	};
+
 	if (credentials) {
 		return (
 			<div className="app">
-				<LoginPage handleSubmit={ setCredentials } />
+				<DataCollectionPage
+					credentials={ credentials }
+					handleSubmit={ submitMatchData }
+				/>
 			</div>
 		);
 	}
 
 	return (
 		<div className="app">
-			<DataCollectionPage credentials={ credentials } handleSubmit={ submitMatchData } />
+			<LoginPage handleSubmit={ handleLogin } />
 		</div>
 	);
 }

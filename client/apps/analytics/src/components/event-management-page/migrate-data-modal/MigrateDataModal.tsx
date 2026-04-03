5b1b9137b54cdde9a9b37b0967942207f008921b
Patrick Ubelhor <pubelhor@gmail.com>
2026-04-03 17:52:56 -0400

    Update modal styling

--- client/apps/analytics/src/components/event-management-page/migrate-data-modal/MigrateDataModal.tsx
+++ client/apps/analytics/src/components/event-management-page/migrate-data-modal/MigrateDataModal.tsx
@@ -1,7 +1,8 @@
+import './MigrateDataModal.scss';
 import { useState } from 'react';
 import { useTranslator } from '../../../service/TranslateService';
 import { useAppDispatch, useAppSelector } from '../../../state';
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
 import { IEventInfo } from '@gearscout/shared-models';
 
 interface IProps {
@@ -26,58 +27,69 @@ export default function MigrateDataModal(props: IProps) {
 	const handleConfirmation = () => {
 		console.log('Confirmed!');
 		// TODO: dispatch data migration action
+		setEventCode('');
+		setSecretCode('');
+		setGameYear('');
 		props.handleClose();
 	};
 
 	const handleCancel = () => {
 		console.log('Canceled!');
+		setEventCode('');
+		setSecretCode('');
+		setGameYear('');
 		props.handleClose();
 	};
 
 	return (
-		<Dialog open={ props.isOpen } onClose={ handleCancel }>
+		<Dialog className="migrate-data-modal" open={ props.isOpen } onClose={ handleCancel }>
 			<DialogTitle>{ translate('ARE_YOU_SURE' )}</DialogTitle>
-			<DialogContentText>
-				This will migrate all of the data from the target event into this one.
-				The target event will be removed once all data has been transferred.
-				Please enter the information for your currently selected event
-				to confirm that you wish to perform this migration.
-			</DialogContentText>
 			<DialogContent>
-				<TextField
-					name="eventCode"
-					placeholder={ translate('EVENT_CODE' )}
-					margin="dense"
-					variant="outlined"
-					autoComplete="off"
-					value={ eventCode }
-					onChange={ event => setEventCode(event.target.value) }
-				/>
-				<TextField
-					name="secretCode"
-					placeholder={ translate('SECRET_CODE') }
-					margin="dense"
-					variant="outlined"
-					autoComplete="off"
-					value={ secretCode }
-					onChange={ event => setSecretCode(event.target.value) }
-				/>
-				<TextField
-					name="gameYear"
-					placeholder={ translate('GAME_YEAR') }
-					type="text"
-					inputMode="numeric"
-					margin="dense"
-					variant="outlined"
-					autoComplete="off"
-					value={ gameYear }
-					onChange={ event => setGameYear(event.target.value) }
-					slotProps={{
-						htmlInput: {
-							pattern: '[0-9]*'
-						}
-					}}
-				/>
+				<div className="modal-content">
+					<div className="prompt">
+						{ translate('DATA_MIGRATION_DESCRIPTION') }
+					</div>
+					<div className="prompt">
+						{ translate('DATA_MIGRATION_PROMPT') }
+					</div>
+					<TextField
+						id="event-code-input"
+						name="eventCode"
+						label={ translate('EVENT_CODE' )}
+						margin="dense"
+						variant="outlined"
+						autoComplete="off"
+						value={ eventCode }
+						onChange={ event => setEventCode(event.target.value) }
+					/>
+					<TextField
+						id="secret-code-input"
+						name="secretCode"
+						label={ translate('SECRET_CODE') }
+						margin="dense"
+						variant="outlined"
+						autoComplete="off"
+						value={ secretCode }
+						onChange={ event => setSecretCode(event.target.value) }
+					/>
+					<TextField
+						id="game-year-input"
+						name="gameYear"
+						label={ translate('GAME_YEAR') }
+						type="text"
+						inputMode="numeric"
+						margin="dense"
+						variant="outlined"
+						autoComplete="off"
+						value={ gameYear }
+						onChange={ event => setGameYear(event.target.value) }
+						slotProps={{
+							htmlInput: {
+								pattern: '[0-9]*'
+							}
+						}}
+					/>
+				</div>
 			</DialogContent>
 			<DialogActions>
 				<Button
--- client/apps/analytics/src/service/TranslateService.tsx
+++ client/apps/analytics/src/service/TranslateService.tsx
@@ -211,10 +211,12 @@ const languages: ILanguageTranslation = {
 		'SHOW_ONLY_VALID_EVENTS': 'Show only valid events',
 		'SHOW_ONLY_HIDDEN_EVENTS': 'Show only hidden events',
 		'ARE_YOU_SURE': 'Are you sure?',
-		'DATA_MIGRATION_CONFIRMATION': 'This will migrate all of the data from the target event into this one.\n' +
-			'The target event will be removed once all data has been transferred.\n' +
-			'Please enter the information for your currently selected event\n' +
-			'to confirm that you wish to perform this migration.'
+		'DATA_MIGRATION_DESCRIPTION': 'This will migrate all of the data from the target event into this one.\n' +
+			'The target event will be removed once all data has been transferred.',
+		'DATA_MIGRATION_PROMPT': 'Please enter the information for your currently selected event\n' +
+			'to confirm that you wish to perform this migration.',
+		'YES': 'Yes',
+		'NO': 'No'
 	},
 	[Language.SPANISH]: {
 		'SIGN_IN': 'Iniciar sesión',

720d46cbdf18b713a12aa914b770ef2c3dfe0873
Patrick Ubelhor <pubelhor@gmail.com>
2026-04-16 13:38:05 -0400

    Reordered lint rules

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -62,22 +62,19 @@ export default [
 			'**/*.cjs',
 			'**/*.mjs'
 		],
-		// Override or add rules here
 		rules: {
 			/* These are probably controlled by Prettier now */
 			indent: ['error', 'tab', { SwitchCase: 1 }],
-			'linebreak-style': ['error', 'unix'],
 			quotes: ['error', 'single'],
 			semi: ['error', 'always'],
+			'linebreak-style': ['error', 'unix'],
+			'no-array-constructor': 'off', // Disable base rule - let TS plugin handle it
 			'no-case-declarations': ['warn'],
 
 			'import/no-duplicates': ['warn', { 'prefer-inline': true }],
 
 			/* TypeScript rules */
-			'@typescript-eslint/array-type': [
-				'error',
-				{ default: 'array' }
-			],
+			'@typescript-eslint/array-type': ['error', { default: 'array' }],
 			'@typescript-eslint/consistent-generic-constructors': [
 				'warn',
 				'type-annotation' // Prefer putting generics on the left side
@@ -89,11 +86,10 @@ export default [
 					objectLiteralTypeAssertions: 'allow'
 				}
 			],
-			'@typescript-eslint/no-inferrable-types': 'off',
+			'@typescript-eslint/no-array-constructor': 'error',
 			'@typescript-eslint/no-explicit-any': 'warn',
+			'@typescript-eslint/no-inferrable-types': 'off',
 			'@typescript-eslint/no-unused-vars': 'warn',
-			'no-array-constructor': 'off', // Disable base rule - let TS plugin handle it
-			'@typescript-eslint/no-array-constructor': 'error'
 		}
 	}
 ];
